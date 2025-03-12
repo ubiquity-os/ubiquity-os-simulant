755bda7f7032129a44de024e677c4596f5153fd4
gentlementlegen <fernand.veyrier@epitech.eu>
2025-03-12 15:15:26 +0900

    chore: test barebones code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import { EmailMessage } from "@cloudflare/workers-types";
-import { customOctokit } from "@ubiquity-os/plugin-sdk/octokit";
+// import { customOctokit } from "@ubiquity-os/plugin-sdk/octokit";
 import { Env } from "./types";
 
-export async function acceptCollaboratorInvitation(owner: string, name: string, env: Env) {
-  const userOctokit = new customOctokit({
-    auth: env.USER_GITHUB_TOKEN,
-  });
-  const { data } = await userOctokit.rest.repos.listInvitationsForAuthenticatedUser();
-  const invite = data.find((o) => o.repository.owner.login === owner && o.repository.name === name);
-  if (invite?.id) {
-    await userOctokit.rest.repos.acceptInvitationForAuthenticatedUser({
-      invitation_id: invite.id,
-    });
-  }
-}
+// export async function acceptCollaboratorInvitation(owner: string, name: string, env: Env) {
+//   const userOctokit = new customOctokit({
+//     auth: env.USER_GITHUB_TOKEN,
+//   });
+//   const { data } = await userOctokit.rest.repos.listInvitationsForAuthenticatedUser();
+//   const invite = data.find((o) => o.repository.owner.login === owner && o.repository.name === name);
+//   if (invite?.id) {
+//     await userOctokit.rest.repos.acceptInvitationForAuthenticatedUser({
+//       invitation_id: invite.id,
+//     });
+//   }
+// }
 
 export default {
   async email(message: EmailMessage & { headers: { get: (s: string) => string } }, env: Env) {
@@ -26,7 +26,8 @@ export default {
       const matches = reg.exec(subject);
       if (matches) {
         const [owner, repo] = matches[1].split("/");
-        await acceptCollaboratorInvitation(owner, repo, env);
+        console.log(owner, repo);
+        // await acceptCollaboratorInvitation(owner, repo, env);
         // await message.forward("ubiquity-os-simulant@ubq.fi");
       }
     } else {

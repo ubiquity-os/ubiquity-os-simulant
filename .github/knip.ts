c78945c617be93ac3dc052ffe4e8aa8b9a1db21c
gentlementlegen <fernand.veyrier@epitech.eu>
2025-03-12 19:28:42 +0900

    chore: fix knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,12 +1,12 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["src/action.ts", "src/worker.ts"],
+  entry: ["src/index.ts"],
   project: ["src/", "test/"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["ts-node", "husky"],
+  ignoreDependencies: ["ts-node", "@ubiquity-os/plugin-sdk", "@ubiquity-os/ubiquity-os-logger"],
   eslint: true,
 };
 
